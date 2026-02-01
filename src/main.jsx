9d3d3b18651f7458df5177329406b32afdc5115c
Aryan Nirmal <aryannirmal@Aryans-MacBook-Air-2.local>
2026-02-01 16:00:41 +0530

    Install Vercel Speed Insights and Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 import { CartProvider } from './context/CartContext'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <CartProvider>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </CartProvider>
     </AuthProvider>
   </StrictMode>,
